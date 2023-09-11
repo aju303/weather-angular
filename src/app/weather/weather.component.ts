31648ba959fde546ac7ed241413df0e0de183bca
ajmal <ahoyaju@gmail.com>
2023-09-11 16:05:41 +0530

    initial commit

---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { WeatherService } from '../services/weather.service';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent {
+
+
+wdata={
+  lname:"",
+  humidity:"",
+  temp:"",
+  windspeed:"",
+  description:""
+
+
+}
+
+  constructor (private service:WeatherService){ }
+
+
+  fetchWeather(inpt:any){
+    console.log(inpt.value);
+    this.service.findWeather(inpt.value).then(res=>res.json()).then(data=>{
+      this.wdata.lname=data.name
+      this.wdata.humidity=data.main.humidity
+      this.wdata.temp=data.main.temp
+      this.wdata.windspeed=data.wind.speed
+      this.wdata.description=data.weather[0].description
+
+    })
+    }
+    
+
+
+    }
+    
+  
+
